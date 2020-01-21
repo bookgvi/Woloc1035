31bce42d950f0585fad9f68e3aaa0d2c42f6b428
book <bookgvi@gmail.com>
2020-01-21 19:45:15 +0300

    UGOLCO-1035: git read-tree --reset -u bb/feature/UGOLOC-1035

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { stringify } from 'qs'
-import { LocalStorage, Notify, Loading, QSpinnerGears } from 'quasar'
+import { LocalStorage, Loading, QSpinnerGears } from 'quasar'
 
 const currentURL = (_ => {
   const currentURL = window.location
@@ -8,11 +8,12 @@ const currentURL = (_ => {
   let isCabinet = currentURL.hostname.split('.')
   if (baseURL === 'localhost') {
     baseURL = 'https://pre.ugoloc.ucann.ru'
-    // baseURL = 'https://ugoloc-1038.shot.ugoloc.ucann.ru'
+    // baseURL = 'https://ugoloc-828.shot.ugoloc.ucann.ru'
   } else if (isCabinet[0] === `cabinet`) {
     isCabinet.shift()
     baseURL = `https://${isCabinet.join('.')}`
   }
+  baseURL = 'https://pre.ugoloc.ucann.ru'
   return `${baseURL}/api`
 })()
 const instance = axios.create({
@@ -64,25 +65,25 @@ instance.interceptors.response.use(
           window.location.href = `/login`
           break
         default:
-          if (response.data && response.data.errors) {
-            if (Array.isArray(response.data.errors)) {
-              response.data.errors.forEach(err => {
-                Notify.create({
-                  message: err.title,
-                  color: 'negative',
-                  position: 'bottom-left',
-                  icon: 'warning'
-                })
-              })
-            } else {
-              Notify.create({
-                message: response.data.errors.title,
-                color: 'negative',
-                position: 'bottom-left',
-                icon: 'warning'
-              })
-            }
-          }
+          // if (response.data && response.data.errors) {
+          //   if (Array.isArray(response.data.errors)) {
+          //     response.data.errors.forEach(err => {
+          //       Notify.create({
+          //         message: err.title,
+          //         color: 'negative',
+          //         position: 'bottom-left',
+          //         icon: 'warning'
+          //       })
+          //     })
+          //   } else {
+          //     Notify.create({
+          //       message: response.data.errors.title,
+          //       color: 'negative',
+          //       position: 'bottom-left',
+          //       icon: 'warning'
+          //     })
+          //   }
+          // }
       }
       return response.data
     }
